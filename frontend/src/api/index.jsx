947553304d2e74de4eb6a34d28ac99608ef6e455
aditya kadam <work.adityakadam@gmail.com>
2024-10-07 11:20:11 +0530

    Last-Commit

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-manager-backend-0fvy.onrender.com",
+  baseURL: "https://task-manager-backend-0fvy.onrender.com/api",
 });
 export default api;
